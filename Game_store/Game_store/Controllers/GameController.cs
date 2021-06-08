c565405c17df815a04920719bf72c1890eb9bf47
denys-u <denys.udovik@nure.ua>
2021-06-08 21:17:54 +0300

    created Game Controller

---
+++ Game_store/Game_store/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using Game_store.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_store.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(IGameService gameService, ILogger<GameController> logger)
+        {
+            _logger = logger;
+            _gameService = gameService;
+        }
+    }
+}
